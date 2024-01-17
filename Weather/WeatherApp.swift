d5ac365a0c6134daac57878b62ecb6244f00cec0
Heshantha Mahawaththage Don <heshantha.don@icloud.com>
2024-01-17 17:28:51 +0000

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Heshantha Don on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
