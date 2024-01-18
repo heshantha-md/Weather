877d4274d32b945558d2198c0c999bbd87d4ed6a
Heshantha Mahawaththage Don <heshantha.don@icloud.com>
2024-01-18 16:33:41 +0000

    Implementing Home Screen

--- Weather/Utils/Extensions.swift
+++ Weather/Utils/Extensions.swift
@@ -5,6 +5,7 @@
 //  Created by Dara To on 2022-05-30.
 //
 
+import Foundation
 import SwiftUI
 
 extension Color {
@@ -16,7 +17,7 @@ extension Color {
     static let bottomSheetBorderMiddle = LinearGradient(gradient: Gradient(stops: [.init(color: .white, location: 0), .init(color: .clear, location: 0.2)]), startPoint: .top, endPoint: .bottom)
     static let bottomSheetBorderTop = LinearGradient(gradient: Gradient(colors: [.white.opacity(0), .white.opacity(0.5), .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
     static let underline = LinearGradient(gradient: Gradient(colors: [.white.opacity(0), .white, .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
-    static let tabBarBorder = Color("Tab Bar Border").opacity(0.5)
-    static let forecastCardBackground = Color("Forecast Card Background")
-    static let probabilityText = Color("Probability Text")
+//    static let tabBarBorder = Color("Tab Bar Border").opacity(0.5)
+//    static let forecastCardBackground = Color("Forecast Card Background")
+//    static let probabilityText = Color("Probability Text")
 }
