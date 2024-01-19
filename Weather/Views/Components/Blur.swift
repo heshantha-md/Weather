e75b923198508c6bd8fc72e2afe40b4d70c44149
Heshantha Mahawaththage Don <heshantha.don@icloud.com>
2024-01-19 13:32:52 +0000

    Implementing Bottom Sheet using BottomSheet Package (https://github.com/Wouter125/BottomSheet)

---
+++ Weather/Views/Components/Blur.swift
@@ -0,0 +1,18 @@
+//
+//  Blur.swift
+//  Weather
+//
+//  Created by Heshantha Don on 19/01/2024.
+//
+
+import SwiftUI
+
+struct Blur: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Blur()
+}
---
+++ Weather/Views/Detail/ForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastView.swift
+//  Weather
+//
+//  Created by Heshantha Don on 19/01/2024.
+//
+
+import SwiftUI
+
+struct ForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForecastView()
+}
