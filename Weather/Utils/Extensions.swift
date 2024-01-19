a24b83cad6da9f351ebe2af00295f1e044a6cbc3
Heshantha Mahawaththage Don <heshantha.don@icloud.com>
2024-01-19 14:38:28 +0000

    Implementing Inner Shadow for Bottom Sheet

--- Weather/Utils/Extensions.swift
+++ Weather/Utils/Extensions.swift
@@ -19,8 +19,8 @@ extension Color {
     static let underline = LinearGradient(gradient: Gradient(colors: [.white.opacity(0), .white, .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
 //    static let tabBarBorder = Color("Tab Bar Border").opacity(0.5)
 //    static let forecastCardBackground = Color("Forecast Card Background")
-//    static let probabilityText = Color("Probability Text")
 }
+//    static let probabilityText = Color("Probability Text")
 
 extension View {
     func backgroundBlur(radius: CGFloat = 3, opaque: Bool = false) -> some View {
@@ -28,5 +28,26 @@ extension View {
             .background( 
                 Blur(radius: radius, opaque: opaque)
             )
+    }
+}
+
+extension View {
+    func innerShadow<S: Shape, SS: ShapeStyle>(shape: S,
+                                               color: SS,
+                                               lineWidth: CGFloat = 1,
+                                               offsetX: CGFloat = 0,
+                                               offsetY: CGFloat = 0,
+                                               blur: CGFloat = 4,
+                                               blendMode: BlendMode = .normal,
+                                               opacity: Double = 1) -> some View {
+        return self
+            .overlay {
+                shape
+                    .stroke(color, lineWidth: lineWidth)
+                    .offset(x: offsetX, y: offsetY)
+                    .blur(radius: blur)
+                    .mask(shape)
+                    .opacity(opacity)
+            }
     }
 }
