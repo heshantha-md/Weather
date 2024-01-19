8bff0987f857586a2c05264e584a2a6749119d74
Heshantha Mahawaththage Don <heshantha.don@icloud.com>
2024-01-19 17:40:14 +0000

    Implementing Animation in Home Screen

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -11,93 +11,120 @@ import BottomSheet
 struct HomeView: View {
     // MARK: - PROPERTIES
     @State var isPresented = true
-    @State var selectedDetent: BottomSheet.PresentationDetent = .medium
+    @State var bottomSheetSelectedDetent: BottomSheet.PresentationDetent = .medium
+    @State var bottomSheetTranslation: CGFloat = BottomSheet.PresentationDetent.medium.size
+    
+    var bottomSheetTranslationProrated: CGFloat {
+        withAnimation {
+             let translationProrated = (bottomSheetTranslation - BottomSheet.PresentationDetent.medium.size) / BottomSheet.PresentationDetent.medium.size
+             return translationProrated > 0.79 ? 1 : translationProrated < 0 ? 0 : translationProrated
+        }
+    }
+    
+    @State var hasDragged = false
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                // MARK: - Background Color
-                Color.background
-                    .ignoresSafeArea()
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height + geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
+                let imageOffSet = screenHeight + 36
                 
-                // MARK: - Background Image
-                Image("Background")
-                    .resizable()
-                    .ignoresSafeArea()
-                
-                // MARK: - House Image
-                Image("House")
-                    .frame(maxHeight: .infinity, alignment: .top)
-                    .padding(.top, 257)
-                
-                VStack {
-                    Text("Montreal")
-                        .font(.largeTitle)
+                ZStack {
+                    // MARK: - Background Color
+                    Color.background
+                        .ignoresSafeArea()
                     
-                    VStack {
-                        Text(attributedString)
+                    // MARK: - Background Image
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationProrated * imageOffSet)
+//                        .opacity(1 - bottomSheetTranslationProrated)
+                    
+                    // MARK: - House Image
+                    Image("House")
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.top, 257)
+                        .offset(y: -bottomSheetTranslationProrated * imageOffSet)
+//                        .opacity(1 - bottomSheetTranslationProrated)
+                    
+                    VStack(spacing: -10 * (1 - bottomSheetTranslationProrated)) {
+                        Text("Montreal")
+                            .font(.largeTitle)
                         
-                        Text("H:24°   L:18°")
-                            .font(.title3.weight(.semibold))
+                        VStack {
+                            Text(attributedString)
+                            
+                            // MARK: - Current Weather
+                            Text("H:24°   L:18°")
+                                .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranslationProrated)
+                        }
+                        
+                        Spacer()
                     }
+                    .padding(.top, 40)
+                    .offset(y: -bottomSheetTranslationProrated * 33)
                     
-                    Spacer()
+                    // MARK: - Bottom Sheet
+                    .sheetPlus(isPresented: $isPresented,
+                               background: Blur(radius: 25, opaque: true)
+                                            .background(Color.bottomSheetBackground)
+                                            .clipShape(RoundedRectangle(cornerRadius: 44))
+                                            .innerShadow(shape: RoundedRectangle(cornerRadius: 44),
+                                                         color: Color.bottomSheetBorderMiddle,
+                                                         lineWidth: 1,
+                                                         offsetX: 0,
+                                                         offsetY: 1,
+                                                         blur: 0,
+                                                         blendMode: .overlay,
+                                                         opacity: 1 - bottomSheetTranslationProrated)
+                                            .overlay {
+                                                // MARK: - Bottom Sheet Separator
+                                                Divider()
+                                                    .blendMode(.overlay)
+                                                    .background(Color.bottomSheetBorderTop)
+                                                    .frame(maxHeight: .infinity, alignment: .top)
+                                                    .clipShape(RoundedRectangle(cornerRadius: 44))
+                                            }
+                                            .overlay {
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .fill(.black.opacity(0.5))
+                                                    .frame(width: 48, height: 5)
+                                                    .frame(height: 20)
+                                                    .frame(maxHeight: .infinity, alignment: .top)},
+                               onDrag: { translation in
+                                   bottomSheetTranslation = translation
+                                   withAnimation(.easeInOut) {
+                                       hasDragged = bottomSheetSelectedDetent == .large
+                                   }
+                               },
+                               header: { EmptyView() },
+                               main: {
+                                    ForecastView()
+                                        .presentationDetentsPlus(
+                                            [.medium, .large],
+                                            selection: $bottomSheetSelectedDetent
+                                        )
+                                        .presentationBackgroundInteractionPlus(.enabled(upThrough: .height(580)))
+                               })
+                    
+                    // MARK: - Tab Bar
+                    TabBar(action: {
+                        bottomSheetSelectedDetent = .large
+                    })
+                    .offset(y: bottomSheetTranslationProrated * 115)
+                    .opacity(1 - bottomSheetTranslationProrated)
                 }
-                .padding(.top, 51)
-                
-                // MARK: - Bottom Sheet
-                .sheetPlus(isPresented: $isPresented,
-                           background: Blur(radius: 25, opaque: true)
-                                        .background(Color.bottomSheetBackground)
-                                        .clipShape(RoundedRectangle(cornerRadius: 44))
-                                        .innerShadow(shape: RoundedRectangle(cornerRadius: 44),
-                                                     color: Color.bottomSheetBorderMiddle,
-                                                     lineWidth: 1,
-                                                     offsetX: 0,
-                                                     offsetY: 1,
-                                                     blur: 0,
-                                                     blendMode: .overlay,
-                                                     opacity: 1)
-                                        .overlay {
-                                            // MARK: - Bottom Sheet Separator
-                                            Divider()
-                                                .blendMode(.overlay)
-                                                .background(Color.bottomSheetBorderTop)
-                                                .frame(maxHeight: .infinity, alignment: .top)
-                                                .clipShape(RoundedRectangle(cornerRadius: 44))
-                                        }
-                                        .overlay {
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .fill(.black.opacity(0.5))
-                                                .frame(width: 48, height: 5)
-                                                .frame(height: 20)
-                                                .frame(maxHeight: .infinity, alignment: .top)},
-                           onDrag: { translation in
-                               print(translation)
-                           },
-                           header: { EmptyView() },
-                           main: {
-                                ForecastView()
-                                    .presentationDetentsPlus(
-                                        [.height(770), .fraction(0.4), .medium, .large],
-                                        selection: $selectedDetent
-                                    )
-                                    .presentationBackgroundInteractionPlus(.enabled(upThrough: .height(325)))
-                           })
-                
-                // MARK: - Tab Bar
-                TabBar(action: {
-                    selectedDetent = .large
-                })
             }
         }
     }
     
     private var attributedString: AttributedString {
-        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        var string = AttributedString("19°" + (hasDragged ? " | " : "\n ") + "Mostly Clear")
         if let temp = string.range(of: "19°") {
-            string[temp].font = .system(size: 96, weight: .thin)
-            string[temp].foregroundColor = .primary
+            string[temp].font = .system(size: 96 - (bottomSheetTranslationProrated * (96 - 20)), weight: hasDragged ? .semibold : .thin)
+            string[temp].foregroundColor = hasDragged ? .secondary : .primary
         }
         
         if let pipe = string.range(of: " | ") {
